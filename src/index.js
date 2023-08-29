342b4d044a972203c06ade34512423c8eeb36b98
Uniquecon <akinwunmiolusegun277@gmail.com>
2023-08-29 13:17:55 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+
+const Default = () => {
+  
+  return (
+    <div>Hello World!</div>
+  );
+};
+
+ReactDOM.render(<Default/>, document.getElementById("root"))
+
